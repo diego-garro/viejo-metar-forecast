a4d64438ef6fd962c75018ed7b40a188b2511e7f
diegogarromolina <diego.garromolina@yahoo.com>
2020-11-21 05:17:00 -0600

    Implemented start year and end year for parse metars, correction on 2005 for MROC metars

--- app.py
+++ app.py
@@ -17,9 +17,18 @@ def adios():
     click.echo('Adiós')
 
 @cliapp.command()
-@click.option('-s', '--station', required=True, type=str)
-def parsemetars(station):
-    parse_metars_and_write_csv(station.upper())
+@click.option('-s', '--station', required=True, type=str, help='The station ICAO code')
+@click.option('-a', '--year-start', type=int, help='The start year to process')
+@click.option('-e', '--year-end', type=int, help='The end year to process')
+def parsemetars(station, year_start, year_end):
+    if year_start is not None and year_end is not None:
+        parse_metars_and_write_csv(station.upper(), year_start=year_start, year_end=year_end)
+    elif year_start is not None:
+        parse_metars_and_write_csv(station.upper(), year_start=year_start)
+    elif year_end is not None:
+        parse_metars_and_write_csv(station.upper(), year_end=year_end)
+    else:
+        parse_metars_and_write_csv(station.upper())
 
 cli = click.CommandCollection(sources=[cliforecast, cliapp])
 
