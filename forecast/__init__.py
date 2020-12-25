d168a95cdd9446da26d6dcda7c22e9a60220b60f
diegogarromolina <diego.garromolina@yahoo.com>
2020-12-24 20:42:26 -0600

    Corrections to MRLB-2005 completed, change name of command parsemetars to parse-metars

--- forecast/__init__.py
+++ forecast/__init__.py
@@ -11,7 +11,7 @@ def cliforecast():
 @click.option('-s', '--station', required=True, type=str, help='The station ICAO code')
 @click.option('-a', '--year-start', type=int, help='The start year to process')
 @click.option('-e', '--year-end', type=int, help='The end year to process')
-def parsemetars(station, year_start, year_end):
+def parse_metars(station, year_start, year_end):
     if year_start is not None and year_end is not None:
         parse_metars_and_write_csv(station.upper(), year_start=year_start, year_end=year_end)
     elif year_start is not None:
